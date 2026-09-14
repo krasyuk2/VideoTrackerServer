8b7f095697f9f3d05c25b1ca2c6c129fd10df942
krasyuk2 <krasyuk.maksimka@gmail.com>
2026-09-14 23:41:04 +0700

    Начало идей вытаскивания типа, имени контента

---
+++ VideoTrackerServer.Domain/Abstractions/ContentVideoTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace VideoTrackerServer.Domain.Abstractions;
+
+/// <summary>
+///     Перечисление типов видео.
+/// </summary>
+public enum ContentVideoTypes
+{
+    Movie,
+    Serial,
+    Anime,
+    Video, // По типу ютуба и тд
+    Unrecognized
+}
---
+++ VideoTrackerServer.Domain/Abstractions/IMediaContentResolverServer.cs
@@ -0,0 +1,9 @@
+﻿namespace VideoTrackerServer.Domain.Abstractions;
+
+/// <summary>
+///     Сервис определения типа контента, и получение его информации
+/// </summary>
+public interface IMediaContentResolverServer
+{
+    
+}
